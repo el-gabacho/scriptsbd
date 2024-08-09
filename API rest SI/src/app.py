130bdc1c0b1441f60acc52839a9be718f6d2dad9
Leonel Valencia <valenciaestudillo@gmail.com>
2024-08-09 15:19:08 -0600

    api usuario terminado

--- API rest SI/src/app.py
+++ API rest SI/src/app.py
@@ -2,6 +2,8 @@ from flask import Flask
 from config import configuracion
 from models import db, ma  # Importar db y ma para inicializarlos
 import routes
+from auth import auth
+
 #cambio
 app = Flask(__name__)
 app.config.from_object(configuracion['development'])
@@ -13,6 +15,7 @@ ma.init_app(app)
 #prueba de cambio 
 # Registrar las rutas desde el Blueprint
 app.register_blueprint(routes.routes)
+app.register_blueprint(auth)
 
 if __name__ == '__main__':
     app.run(debug=True)
