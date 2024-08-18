3518d365a09d90133d332fe0550c9ece9b10b1ac
Leonel Valencia <valenciaestudillo@gmail.com>
2024-08-18 09:07:03 -0600

    subir imagenes en config

--- API_rest_SI/src/configuracion/Infrastructure/routes.py
+++ API_rest_SI/src/configuracion/Infrastructure/routes.py
@@ -1,6 +1,8 @@
 from flask import request, jsonify
 from configuracion.Application.funciones import obtenerConfiguracion, actualizarConfiguracion, actualizarTelefonos, actualizarHorario
 from configuracion import config as routes
+from werkzeug.utils import secure_filename
+import os
 
 @routes.route('/configuracion', methods=['GET'])
 def getConfiguracion():
@@ -25,5 +27,24 @@ def updateConfiguracion():
         
         return jsonify(resultados)
     except Exception as e:
-        print(e)
         return jsonify({'error': 'Error al actualizar la configuracion'}), 500
+
+@routes.route('/configuracion/imagenes', methods=['POST'])
+def upload_files():
+    try:
+        if 'files' not in request.files:
+            return jsonify({'message': 'No file part in the request'}), 400
+
+        files = request.files.getlist('files')
+
+        for file in files:
+            if file.filename == '':
+                return jsonify({'message': 'No file selected for uploading'}), 400
+
+            if file:
+                filename = secure_filename(file.filename)
+                file.save(os.path.join('C:\\Users\\VALENCIA\\Documents\\proyectos\\el-gabacho\\images', filename))
+    except Exception as e:
+        print(e)
+        return jsonify({'message': 'Allowed file types are .webp'}), 400
+    return jsonify({'message': 'Files successfully uploaded'}), 200
