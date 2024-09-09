7a036bd715cd3eec4fdea727c0b9fa2e44c7459b
Pxcheco <132029761+Pxcheco@users.noreply.github.com>
2024-09-09 17:39:10 -0600

    Mensajes nuevo en login iniciar sesion

--- API_rest_SI/src/auth/funciones.py
+++ API_rest_SI/src/auth/funciones.py
@@ -1,5 +1,5 @@
 from usuarios.modelos import Usuario
-from flask import jsonify, make_response
+from flask import jsonify
 
 def iniciar_sesion(username, password):
     sesion = {}
@@ -9,15 +9,15 @@ def iniciar_sesion(username, password):
     
     # Verificar si el usuario no existe o si la contraseña es incorrecta
     if user is None or not user.check_password(password):
-        return make_response(jsonify({'error': 'Credenciales incorrectas'}), 401)
+        return jsonify({'Error':'Las credenciales que has ingresado no son correctas. Por favor, verifica tu nombre de usuario y contraseña e inténtalo de nuevo.'}), 401
     
     # Verificar si el usuario está deshabilitado
     if user.estado == 0:
-        return make_response(jsonify({'error': 'El usuario está deshabilitado'}), 403)
+        return jsonify({'Error' : 'No tienes permisos para acceder a esta sección, tu cuenta está deshabilitada. Contacta con el administrador para más detalles.'}), 403
     
     # Verificar si el usuario tiene un idRol no permitido (idRol == 3)
     if user.idRol == 3:
-        return make_response(jsonify({'error': 'No tienes permisos para acceder a esta sección'}), 403)
+        return jsonify({'Error': 'No tienes permisos para acceder a esta sección. Contacta con el administrador para más detalles.'}), 403
     
     # Verificar si el idRol es 1 o 2 (roles permitidos)
     if user.idRol in [1, 2]:
